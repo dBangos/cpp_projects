445333cbffe6ffae06157d5b83f3aa5ecaac8a7e
Dimitrios Bangos <sleireth@gmail.com>
2020-10-31 18:08:48 +0200

    Counts the times 1 shows up in a 32bit unsigned int

---
+++ src/ergasia1_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+ 
+static const int Tests=20;
+ 
+int counting_function(unsigned int a){
+	unsigned short int counter=0;
+	while(a>=1){
+		if (a%2==1) ++counter;
+		a=a/2;	
+	}
+	return counter;
+}
+ 
+int main(){
+	unsigned int Test_array [Tests];
+	std::srand(std::time(NULL));
+	for (int i = 0; i < Tests; ++i){
+		Test_array[i]=std::rand();
+		std::cout<<Test_array[i]<<std::endl;
+		std::cout<<counting_function(Test_array[i])<<std::endl;
+
+	}
+}
