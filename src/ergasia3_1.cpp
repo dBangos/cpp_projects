e7e2efd7d5467247b697706683c928517d95cbba
Dimitrios Bangos <sleireth@gmail.com>
2020-11-06 04:40:30 +0200

    RunLength Encode Class Implementation

--- src/ergasia3_1.cpp
+++ src/ergasia3_1.cpp
@@ -18,32 +18,29 @@ public:
         outint=0;
         outcount=0;
     }
-    void run(ac_channel<dtype> &in, ac_channel<dtype> &out){
-        if(in.available(1)){        
-            while(in.available(1)){
-                data=in.read();
-                //std::cout<<"Data in read: "<<data<<std::endl;
-                if(outcount==0){//an den uparxei prohgoumeno gia na sugrinw
-                    outint=data;
+    void run(ac_channel<dtype> &in, ac_channel<dtype> &out){   
+        while(in.available(1)){
+            data=in.read();
+            //std::cout<<"Data in read: "<<data<<std::endl;
+            if(outcount==0){//an den uparxei prohgoumeno gia na sugrinw
+                outint=data;
+                outcount=1;
+            }
+            else{//an uparxei prohgoumeno
+                if(data==outint){//an to prohgoumeno pshfio einai idio me to twrino
+                    ++outcount;
+                }
+                else{//an to prohgoumeno einai diaforetiko
+                    out.write(outint);
+                    out.write(outcount);//pernw tis prohgoumenes times sto outstream
+                    //std::cout<<"Data out read: "<<outint<<std::endl;
+                    //std::cout<<"Count out read: "<<outcount<<std::endl;
+                    outint=data;//kai grafw tiw kainourgies times stis topikes metavlhtes
                     outcount=1;
                 }
-                else{//an uparxei prohgoumeno
-                    if(data==outint){//an to prohgoumeno pshfio einai idio me to twrino
-                        ++outcount;
-                    }
-                    else{//an to prohgoumeno einai diaforetiko
-                        out.write(outint);
-                        out.write(outcount);//pernw tis prohgoumenes times sto outstream
-                        //std::cout<<"Data out read: "<<outint<<std::endl;
-                        //std::cout<<"Count out read: "<<outcount<<std::endl;
-                        outint=data;//kai grafw tiw kainourgies times stis topikes metavlhtes
-                        outcount=1;
-                    }
-                }
-            }       
-        }
+            }
+        }       
     }//void run
-
 };//class
 
 int main(){
